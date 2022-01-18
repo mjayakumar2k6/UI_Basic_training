97bd6fb4a9d7bcfe5992d9f553d38d41fcde02db
jk <jk>
2022-01-18 07:58:25 +0530

    Class, style and event binding

--- angular/helloworld/src/app/newcomponent/newcomponent.component.ts
+++ angular/helloworld/src/app/newcomponent/newcomponent.component.ts
@@ -10,9 +10,28 @@ export class NewcomponentComponent implements OnInit {
   myid:string = "myinputid";
   idinput:string = "idinput";
   isdisabled:boolean = false;
+  success:string = "success";
+  isSuccess: boolean = false;
+  isSpecial: boolean = true;
+  greet:string = "";
+  successClass: string = "success";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  logMessage() {
+    console.log("Log from angular");
+    this.greet = "Welcome to angular training";
+  }
+
+  greetWithParams(trainingname:string="ANGULAR") {
+    this.greet = `Welcome to ${trainingname} training`
+  }
+
+  eventKeyword(event: MouseEvent) {
+    console.log(event.target);
+    //event.stopPropagation();
+    //event.preventDefault();
+  }
 }
